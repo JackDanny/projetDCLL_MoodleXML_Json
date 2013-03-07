bda93849edb62c78b600d99c8162de688cb33037
Florent Mouysset <mouysset.florent@gmail.com>
2013-03-07 16:54:48 +0100

    Json tests

--- projet/src/main/java/jsonToXml/jsonParser/JsonParserImpl.java
+++ projet/src/main/java/jsonToXml/jsonParser/JsonParserImpl.java
@@ -1,5 +1,44 @@
 package jsonToXml.jsonParser;
 
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+
+
 public class JsonParserImpl {
 
+	public void lecture(){
+		Reader reader= null;
+		JSONObject o = null;
+		try {
+			reader = new FileReader("");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}				
+		
+		JSONTokener jsonT = new JSONTokener(reader);
+
+		try {
+			o = new  JSONObject(jsonT);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		o.get(key);
+		
+		
+		
+		
+		
+		
+	}
+	
 }
