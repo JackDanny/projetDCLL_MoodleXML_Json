9700e1417b60d138c918f1abe2c3bd5124eeb745
Florent Mouysset <mouysset.florent@gmail.com>
2013-03-07 17:53:06 +0100

    Supp Jackson. Deb Json parserToXml

--- projet/src/main/java/jsonToXml/jsonParser/JsonParser.java
+++ projet/src/main/java/jsonToXml/jsonParser/JsonParser.java
@@ -1,5 +1,14 @@
 package jsonToXml.jsonParser;
 
+import org.json.JSONArray;
+
 public interface JsonParser {
 
+/**
+ * @param filename : Json filename
+ * 
+ * @return a {@link JSONArray} of questions
+ * */
+	public JSONArray parser(String filename);
+	
 }
--- projet/src/main/java/jsonToXml/jsonParser/JsonParserImpl.java
+++ projet/src/main/java/jsonToXml/jsonParser/JsonParserImpl.java
@@ -5,19 +5,32 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.Reader;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
 
 
-public class JsonParserImpl {
+public class JsonParserImpl implements JsonParser {
 
+	public static void main(String[] args) {
+		
+		 JsonParserImpl i = new JsonParserImpl();
+		
+		 i.lecture();
+		 
+		 
+	}
+	
 	public void lecture(){
 		Reader reader= null;
 		JSONObject o = null;
+		JSONArray i = null;
+
+		
 		try {
-			reader = new FileReader("");
+			reader = new FileReader("src/test/resources/user.json");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -32,13 +45,38 @@ public class JsonParserImpl {
 			e.printStackTrace();
 		}
 		
-		o.get(key);
+		JSONObject t= null;
+		try {
+			// t= o.get("menu");
+			 t = o.getJSONObject("quiz");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JSONArray t2 = null;
+		
+		try {
+			t2 = t.getJSONArray("question");
+		} catch (JSONException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		
+		try {
+			System.out.println(i.get(0).toString());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
-		
-		
-		
-		
+	}
+
+	public JSONArray parser(String filename) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
