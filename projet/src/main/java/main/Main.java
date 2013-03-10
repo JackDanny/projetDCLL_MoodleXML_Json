e22316971db0e11289169bcd2b82b069ffe46a1d
RaphaelFOURQUET <raphikifourque@gmail.com>
2013-03-10 14:17:14 +0100

    ToJson

--- projet/src/main/java/main/Main.java
+++ projet/src/main/java/main/Main.java
@@ -16,8 +16,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("deb main");
 		XmlParserImpl xmlparser = new XmlParserImpl();
-		System.out.println("deb main");
 		xmlparser.parser(args[0]);
+		System.out.println("fin main");
 	}
 
 }
