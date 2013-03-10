df9763e7c96392233ea06ed22b43995646830757
Florent Mouysset <mouysset.florent@gmail.com>
2013-03-10 18:57:05 +0100

    Nettoyage du XMLWriter

--- projet/src/main/java/jsontoxml/xmlWriter/XmlWriterImpl.java
+++ projet/src/main/java/jsontoxml/xmlWriter/XmlWriterImpl.java
@@ -1,9 +1,6 @@
 package jsontoxml.xmlWriter;
 
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.Reader;
-import java.util.Iterator;
 
 import org.jdom2.Content;
 import org.jdom2.Document;
@@ -11,9 +8,7 @@ import org.jdom2.Element;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 
 public class XmlWriterImpl implements XmlWriter{
 
@@ -50,14 +45,6 @@ public class XmlWriterImpl implements XmlWriter{
 			e.printStackTrace();
 		}
 	}
-	
-	public static void main(String args[]){
-		XmlWriterImpl i = new XmlWriterImpl();
-		JSONArray tab = i.t();		
-     	i.writeXmlToJson(tab, "outXML-file.xml");	
-     	//i.test();
-     	
-	}
 
 	private String convertClassName(String type) {
 		char [] tab = type.toCharArray();
@@ -65,32 +52,6 @@ public class XmlWriterImpl implements XmlWriter{
 		return new String(tab);
 	}
 
-	private void test() {
-		JSONArray tab = t();
-		//System.out.println(tab.length());
-		try {
-			Object o = tab.get(0);		
-			JSONObject o2 = (JSONObject) o;
-			
-			Iterator<?> it = o2.keys() ;
-			//it.next();
-			while(it.hasNext()){
-				//System.out.println( o2.get(  it.toString() ));
-				System.out.println("> " + it.next());
-			//	System.out.println(o2.has("generalfeedback"));
-				//it.next();
-			}
-			
-		//	o2.
-			
-			Object o3 = o2.get("type");
-			System.out.println(o3.toString());
-		
-		} catch (JSONException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-	}
 	
 	private void enregistre(String fichier)
 	{
@@ -107,25 +68,6 @@ public class XmlWriterImpl implements XmlWriter{
 	   catch (java.io.IOException e){}
 	}
 	
-	
-	private JSONArray  t(){
-		Reader reader= null;
-		JSONObject o2 = null;
-		JSONArray t2 = null;
-		JSONObject tbis= null;
-		
-		try {
-			reader = new FileReader("src/test/resources/TrueFalse.json");		
-			//reader = new FileReader("src/test/resources/description_question.json");
-			JSONTokener jsonT = new JSONTokener(reader);			
-			o2 = new  JSONObject(jsonT);
-			tbis = o2.getJSONObject("quiz");
-			t2 = tbis.getJSONArray("question");
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		return t2; 
-	}
 
 
 	public void writeXmlToJson(JSONObject oneQuestion, String nameXmlFileOut) {
